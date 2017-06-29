626946f59f5ff65b1b587f42f1234726d3d136b0
huangfan <huangfan@yy.com>
2017-06-29 21:12:25 +0800

    ffmpeg执行命令同步到存储

--- pillar/jni/ffmpeg.c
+++ pillar/jni/ffmpeg.c
@@ -4802,6 +4802,7 @@ int ffmpeg_exec(int argc, char **argv)
     if ((decode_error_stat[0] + decode_error_stat[1]) * max_error_rate < decode_error_stat[1])
         exit_program(69);
 
+    sync();
     exit_program(received_nb_signals ? 255 : main_return_code);
     return main_return_code;
 }
